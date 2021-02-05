c71789e477c62c0ac076512969a04a606914c425
You Song Hou <yousonghou@yahoo.com>
2021-02-04 23:15:41 -0500

    add a function to load blockchain data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 import Web3 from 'web3';
 
 import './App.css';
+import RestaurantsBlockchain from './abis/Restaurants.json';
 import Restaurants from './components/Restaurants';
 import Restaurant from './components/Restaurant';
 
 class App extends Component{
+  state = {
+    account: '',
+    restaurantsBlockchain: null
+  }
+
   async componentWillMount(){
     await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = RestaurantsBlockchain.networks[networkId];
+
+    if(networkData){
+      const abi = RestaurantsBlockchain.abi;
+      const address = RestaurantsBlockchain.networks[networkId].address;
+
+      const restaurantsBlockchain = new web3.eth.Contract(abi, address);
+      this.setState({ restaurantsBlockchain });
+    }else{
+      window.alert('Contract is not deployed to detected network')
+    }
   }
 
   async loadWeb3(){
