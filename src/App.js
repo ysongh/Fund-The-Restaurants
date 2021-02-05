2883b29132a9436aa7c6f5b091e7a9d2384279c5
You Song Hou <yousonghou@yahoo.com>
2021-02-04 20:09:41 -0500

    install react-router-dom and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import Web3 from 'web3';
 
 import './App.css';
@@ -24,9 +25,9 @@ class App extends Component{
 
   render(){
     return (
-      <div className="App">
+      <Router className="App">
         <h1>Fund The Restaurants</h1>
-      </div>
+      </Router>
     );
   }
 }
