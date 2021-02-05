b2acc43ead4cbd1eae35f7e0559bed4bf6ed0129
You Song Hou <yousonghou@yahoo.com>
2021-02-04 22:47:18 -0500

    add a function to create a new restaurant on blockchain

--- contracts/Restaurants.sol
+++ contracts/Restaurants.sol
@@ -10,7 +10,25 @@ contract Restaurants {
     string name;
     string description;
     string location;
+    string imageURL;
     uint donationGain;
     address payable owner;
+  }
+
+  event RestaurantCreated (
+    uint restaurantId,
+    string name,
+    string description,
+    string location,
+    string imageURL,
+    uint donationGain,
+    address payable owner
+  );
+
+  function createRestaurant(string memory _name, string memory _description, string memory _location, string memory _imageURL, uint _donationGain) public {
+    restaurantCount++;
+
+    restaurants[restaurantCount] = Restaurant(restaurantCount, _name, _description, _location, _imageURL, _donationGain, msg.sender);
+    emit RestaurantCreated(restaurantCount, _name, _description, _location, _imageURL, _donationGain, msg.sender);
   }
 }
--- test/Restaurants.js
+++ test/Restaurants.js
@@ -27,4 +27,46 @@ contract('Restaurants', ([deployer, account1]) => {
             assert.equal(name, "Restaurants");
         });
     });
+
+    describe('restaurant', async() => {
+        let result;
+        let restaurantCount;
+
+        const restaurantName = "Some Company";
+        const restaurantDescription = "This is a restaurant";
+        const restaurantLocation = "123 Test St";
+        const restaurantImageURL = "https:image1dio23jipadijai.com";
+        const restaurantDonationGain = 1;
+
+        before(async() => {
+            result = await restaurants.createRestaurant(restaurantName, restaurantDescription, restaurantLocation, restaurantImageURL, restaurantDonationGain, { from: account1 });
+            restaurantCount = await restaurants.restaurantCount();
+        });
+
+        it('create restaurant', async() => {
+            const event = result.logs[0].args;
+            assert.equal(event.restaurantId.toNumber(), restaurantCount.toNumber(), 'Id is correct');
+            assert.equal(event.name, restaurantName, 'Name is correct');
+            assert.equal(event.description, restaurantDescription, 'Description is correct');
+            assert.equal(event.location, restaurantLocation, 'Location is correct');
+            assert.equal(event.imageURL, restaurantImageURL, 'Image URL is correct');
+            assert.equal(event.donationGain, restaurantDonationGain, 'Donation amount is correct');
+            assert.equal(event.owner, account1, 'Owner is correct');
+        });
+
+        it('has correct restaurant count', async() => {
+            assert.equal(restaurantCount, 1);
+        });
+
+        it('has correct list of restaurants', async() => {
+            const restaurant = await restaurants.restaurants(restaurantCount);
+            assert.equal(restaurant.restaurantId.toNumber(), restaurantCount.toNumber(), 'Id is correct');
+            assert.equal(restaurant.name, restaurantName, 'Name is correct');
+            assert.equal(restaurant.description, restaurantDescription, 'Description is correct');
+            assert.equal(restaurant.location, restaurantLocation, 'Location is correct');
+            assert.equal(restaurant.imageURL, restaurantImageURL, 'Image URL is correct');
+            assert.equal(restaurant.donationGain, restaurantDonationGain, 'Donation amount is correct');
+            assert.equal(restaurant.owner, account1, 'Owner is correct');
+        });
+    });
 })
