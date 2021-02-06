7ef62aa3165d4c0a8786a1b87b049f65b357e34b
You Song Hou <yousonghou@yahoo.com>
2021-02-05 20:06:30 -0500

    add a function to transfer ETH to restaurant owner

--- contracts/Restaurants.sol
+++ contracts/Restaurants.sol
@@ -11,7 +11,7 @@ contract Restaurants {
     string description;
     string location;
     string imageURL;
-    uint donationGain;
+    uint donationNeeded;
     address payable owner;
   }
 
@@ -21,14 +21,33 @@ contract Restaurants {
     string description,
     string location,
     string imageURL,
-    uint donationGain,
+    uint donationNeeded,
     address payable owner
   );
 
-  function createRestaurant(string memory _name, string memory _description, string memory _location, string memory _imageURL, uint _donationGain) public {
+  event DonationForRestaurant (
+    uint restaurantId,
+    uint amount,
+    uint donationNeeded,
+    address from,
+    address payable owner
+  );
+
+  function createRestaurant(string memory _name, string memory _description, string memory _location, string memory _imageURL, uint donationNeeded) public {
     restaurantCount++;
 
-    restaurants[restaurantCount] = Restaurant(restaurantCount, _name, _description, _location, _imageURL, _donationGain, msg.sender);
-    emit RestaurantCreated(restaurantCount, _name, _description, _location, _imageURL, _donationGain, msg.sender);
+    restaurants[restaurantCount] = Restaurant(restaurantCount, _name, _description, _location, _imageURL, donationNeeded, msg.sender);
+    emit RestaurantCreated(restaurantCount, _name, _description, _location, _imageURL, donationNeeded, msg.sender);
+  }
+
+  function donateETHToRestaurant(uint _restaurantId) public payable {
+    Restaurant memory _restaurant = restaurants[_restaurantId];
+
+    _restaurant.owner.transfer(msg.value);
+
+    _restaurant.donationNeeded -= msg.value;
+    restaurants[_restaurantId] = _restaurant;
+
+    emit DonationForRestaurant(_restaurantId, msg.value, _restaurant.donationNeeded, msg.sender, _restaurant.owner);
   }
 }
--- test/Restaurants.js
+++ test/Restaurants.js
@@ -6,7 +6,7 @@ function tokensToWei(val) {
     return web3.utils.toWei(val, 'ether');
 }
 
-contract('Restaurants', ([deployer, account1]) => {
+contract('Restaurants', ([deployer, account1, account2]) => {
     let restaurants;
 
     before(async() => {
@@ -36,10 +36,10 @@ contract('Restaurants', ([deployer, account1]) => {
         const restaurantDescription = "This is a restaurant";
         const restaurantLocation = "123 Test St";
         const restaurantImageURL = "https:image1dio23jipadijai.com";
-        const restaurantDonationGain = 1;
+        const restaurantDonationNeeded = '10000000000000000000';
 
         before(async() => {
-            result = await restaurants.createRestaurant(restaurantName, restaurantDescription, restaurantLocation, restaurantImageURL, restaurantDonationGain, { from: account1 });
+            result = await restaurants.createRestaurant(restaurantName, restaurantDescription, restaurantLocation, restaurantImageURL, restaurantDonationNeeded, { from: account1 });
             restaurantCount = await restaurants.restaurantCount();
         });
 
@@ -50,7 +50,7 @@ contract('Restaurants', ([deployer, account1]) => {
             assert.equal(event.description, restaurantDescription, 'Description is correct');
             assert.equal(event.location, restaurantLocation, 'Location is correct');
             assert.equal(event.imageURL, restaurantImageURL, 'Image URL is correct');
-            assert.equal(event.donationGain, restaurantDonationGain, 'Donation amount is correct');
+            assert.equal(event.donationNeeded, restaurantDonationNeeded, 'Donation needed is correct');
             assert.equal(event.owner, account1, 'Owner is correct');
         });
 
@@ -65,8 +65,46 @@ contract('Restaurants', ([deployer, account1]) => {
             assert.equal(restaurant.description, restaurantDescription, 'Description is correct');
             assert.equal(restaurant.location, restaurantLocation, 'Location is correct');
             assert.equal(restaurant.imageURL, restaurantImageURL, 'Image URL is correct');
-            assert.equal(restaurant.donationGain, restaurantDonationGain, 'Donation amount is correct');
+            assert.equal(restaurant.donationNeeded, restaurantDonationNeeded, 'Donation needed is correct');
             assert.equal(restaurant.owner, account1, 'Owner is correct');
+        });
+    });
+
+    describe('donate to restaurant ', async() => {
+        let result;
+        let restaurant;
+        let restaurantId = 1;
+        let oldDonationNeed;
+        
+        before(async() => {
+            restaurant = await restaurants.restaurants(restaurantId);
+            oldDonationNeed = restaurant.donationNeeded;
+        });
+        
+        it('received correct amount', async() => {
+            let oldDonatorBalanace = await web3.eth.getBalance(account1);
+            oldDonatorBalanace = new web3.utils.BN(oldDonatorBalanace);
+
+            result = await restaurants.donateETHToRestaurant(restaurantId, { from: account2, value: tokensToWei('1') });
+            
+            let newDonatorBalanace = await web3.eth.getBalance(account1);
+            newDonatorBalanace = new web3.utils.BN(newDonatorBalanace);
+
+            let amount = tokensToWei('1');
+            amount = new web3.utils.BN(amount);
+
+            const expectedBalance = oldDonatorBalanace.add(amount);
+
+            assert.equal(newDonatorBalanace.toString(), expectedBalance.toString());
+
+            oldDonationNeed -= +tokensToWei('1');
+            
+            const event = result.logs[0].args;
+            assert.equal(event.restaurantId, restaurantId, 'Restaurant Id is correct');
+            assert.equal(event.amount.toString(), tokensToWei('1'), 'Amount is correct');
+            assert.equal(event.donationNeeded.toString(), oldDonationNeed, 'Donation needed is correct');
+            assert.equal(event.from, account2, 'Donator address is correct');
+            assert.equal(event.owner, account1, 'Owner address is correct');
         });
     });
 })
