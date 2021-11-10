5ac21bff41e329228ab37b773567d1721c8c9aae
You Song Hou <yousonghou@gmail.com>
2021-11-10 00:43:39 -0500

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route  } from 'react-router-dom';
 import Portis from '@portis/web3';
 import Web3 from 'web3';
 
